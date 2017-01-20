f0b292a6192bb245a2c3265dd4e92feac280e93c
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-01-20 11:33:30 -0600

    mvp submission for completed exercise

--- cards.js
+++ cards.js
@@ -1,12 +1,22 @@
 var createButton = document.getElementById("create-button");
-var deleteButton = document.getElementById("delete-button");
-var userInput = document.getElementById("card-input").value;
-
-createButton.addEventListener('click', createCard);
 
 function createCard() {
+    var userInput = document.getElementById("card-input").value;
     var card = document.createElement("DIV");
-    var cardText = document.createTextNode(userInput);
-    card.appendChild(cardText);
-    document.getElementById("main").appendChild()
+    card.classList.add("user-card")
+    var cardText = "<article><h1>" + userInput + "</h1></article><button class='delete-button'>Delete</button>";
+    card.innerHTML += cardText;
+    document.getElementById("main-content").appendChild(card);
+    // delete button actions
+    var deleteButtons = document.getElementsByClassName("delete-button");
+    for (button in deleteButtons) {
+        deleteButtons.item(button).addEventListener('click', deleteCard);
+    }
 }
+
+
+function deleteCard() {
+    this.parentElement.remove();
+}
+
+createButton.addEventListener('click', createCard);
